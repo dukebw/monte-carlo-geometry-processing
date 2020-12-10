b05bb76a2de4ea555cff458be9bd15e18702020c
Brendan Duke <brendanw.duke@gmail.com>
2020-12-09 21:18:03 -0500

    WIP report

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# monte-carlo-geometry-processing
+# Monte Carlo Geometry Processing: A Grid-Free Approach to PDE-Based Methods on Volumetric Domains
+
+I implemented a bare-bones version of the core idea from "Monte Carlo Geometry
+Processing: A Grid-Free Approach to PDE-Based Methods on Volumetric Domains" by
+Sawhney and Crane.
+Suppose we want to solve a PDE like the Laplace or Poisson equation on a geometric structure.
+Sawhney and Crane’s idea is to avoid meshing challenging geometric structures with finite-element methods (FEM).
+FEM can be time-consuming for detailed objects such as the fly on the left (below).
+Instead, they use Monte Carlo integration to find an approximate solution.
+The Monte Carlo approximation is noisy at first, but can be refined by taking more samples, as shown on the right (below).
+
+![](images/mcgp-1-Figure1-1.png)
+(Figure 1 Sawhney & Crane 2020)
+
+
+## Background
+
+In my implementation I focused on solving the Laplace equation.
+
+$$
+\Delta u = 0 \,\textrm{on}\,\Omega
+u = g \,\textrm{on}\,\partial\Omega
+$$
+
+To solve the Laplace equation, Sawhney and Crane use a technique called "walk on spheres".
+The idea uses the fact that the solution of a harmonic function at point p is
+the expected value of the first point on the boundary reached by a random walk
+starting at p.
+By symmetry, we can find this boundary point by sampling uniformly on spheres.
+We start at x0, then successively sample boundary point until we get to `x_k`,
+which is within some threshold $\partial\Omega_\epsilon$ of the boundary (illustrated below).
+
+![](images/mcgp-3-Figure3-1.png)
+(Figure 3 Sawhney & Crane 2020)
+
+
+## Code
+
+I implemented two versions of walk on spheres: a 2D version `walk_on_circles` and a 3D version `walk_on_spheres`.
--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #define EIGEN_NO_DEBUG
+#include "./walk_on_circles.h"
+#include "./walk_on_spheres.h"
 #include "Eigen/Sparse"
 #include "cxxopts.hpp"
 #include "igl/boundary_facets.h"
@@ -7,6 +9,7 @@
 #include "igl/jet.h"
 #include "igl/min_quad_with_fixed.h"
 #include "igl/opengl/glfw/Viewer.h"
+#include "igl/point_mesh_squared_distance.h"
 #include "igl/readOFF.h"
 #include "igl/setdiff.h"
 #include "igl/slice.h"
@@ -19,51 +22,110 @@
 #include <iostream>
 #include <random>
 
-static Eigen::MatrixXd V;
+static Eigen::MatrixXf V;
 static Eigen::MatrixXi F;
-static Eigen::VectorXd Z;
-static Eigen::MatrixXd boundary_segments;
+static Eigen::VectorXf Z;
+static Eigen::MatrixXf boundary_segments;
+static Eigen::MatrixXi boundary_edges;
 static uint32_t n_verts;
 static int max_steps;
 static int num_walks;
 static uint32_t num_updates;
 static bool use_camel;
 
+/**
+ * NOTE(brendan): Solve Laplace equation using walk on spheres in 3D.
+ * Use the z coordinate of the boundary points as boundary condition.
+ */
 static float
-boundary_condition(const Eigen::Vector2d& x)
+walk_on_spheres(int Vidx)
+{
+
+        float stopping_tolerance = 0.05f;
+
+        std::random_device rd;
+        std::mt19937 gen{ rd() };
+        std::normal_distribution<float> dis{ 0.0f, 1.0f };
+
+        float accum = 0.0f;
+        int num_successful_walks = 0;
+        Eigen::Vector3f x0 = V.row(Vidx);
+        for (int i = 0; i < num_walks; i++) {
+                Eigen::MatrixXf x{ 1, 3 };
+                x.row(0) = x0;
+
+                for (int steps = 0; steps < max_steps; ++steps) {
+                        float radius = std::numeric_limits<float>::max();
+
+                        Eigen::VectorXf squared_distances;
+                        Eigen::VectorXi closest_edge;
+                        Eigen::MatrixXf closest_points;
+                        igl::point_mesh_squared_distance(x,
+                                                         V,
+                                                         boundary_edges,
+                                                         squared_distances,
+                                                         closest_edge,
+                                                         closest_points);
+
+                        radius = sqrtf(squared_distances(0));
+                        if (radius < stopping_tolerance) {
+                                accum += closest_points(0, 2);
+                                ++num_successful_walks;
+                                break;
+                        }
+
+                        /**
+                         * NOTE(brendan): sample from a three-dimensional Gaussian.
+                         * This is equivalent to sampling a 3-vector with a random
+                         * orientation in space.
+                         */
+                        Eigen::Vector3f dx{ dis(gen), dis(gen), dis(gen) };
+                        dx /= dx.norm() + 0.0001f;
+                        x.row(0) += radius * dx;
+                }
+        }
+
+        if (num_successful_walks == 0)
+                return 0.0f;
+
+        return accum / num_successful_walks;
+}
+
+static float
+boundary_condition(const Eigen::Vector2f& x)
 {
         return fmod((6.0f * x.array()).floor().sum(), 2.0f);
 }
 
 static float
-closest_point_on_segment(const Eigen::MatrixXd& bs,
-                         const Eigen::Vector2d& x,
+closest_point_on_segment(const Eigen::MatrixXf& bs,
+                         const Eigen::Vector2f& x,
                          int seg_idx,
                          float R)
 {
-        Eigen::Vector2d seg0{ bs(seg_idx, 0), bs(seg_idx, 1) };
-        Eigen::Vector2d seg1{ bs(seg_idx, 2), bs(seg_idx, 3) };
+        Eigen::Vector2f seg0{ bs(seg_idx, 0), bs(seg_idx, 1) };
+        Eigen::Vector2f seg1{ bs(seg_idx, 2), bs(seg_idx, 3) };
 
-        Eigen::Vector2d u = seg1 - seg0;
+        Eigen::Vector2f u = seg1 - seg0;
         float t = ((x - seg0).transpose() * u);
         t /= u.squaredNorm();
         t = std::clamp(t, 0.0f, 1.0f);
 
-        Eigen::Vector2d p = ((1 - t) * seg0) + (t * seg1);
+        Eigen::Vector2f p = ((1 - t) * seg0) + (t * seg1);
         float distance = (x - p).norm();
 
         return std::min(R, distance);
 }
 
 /**
- * NOTE(brendan): Solve Laplace equation using walk on spheres.
+ * NOTE(brendan): Solve Laplace equation using walk on spheres in 2D.
  * Give the boundary condition in piecewise segments (bndary_segs).
  * Pass a function g to evaluate on the boundary.
  */
 static float
-walk_on_circles(const Eigen::Vector2d& x0,
-                const Eigen::MatrixXd& bndary_segs,
-                const std::function<float(const Eigen::Vector2d&)> g,
+walk_on_circles(const Eigen::Vector2f& x0,
+                const Eigen::MatrixXf& bndary_segs,
+                const std::function<float(const Eigen::Vector2f&)> g,
                 int num_walks,
                 int max_steps)
 {
@@ -74,13 +136,12 @@ walk_on_circles(const Eigen::Vector2d& x0,
         std::uniform_real_distribution<float> dis{ 0.0f, 2.0f * igl::PI };
 
         float accum = 0.0f;
-        Eigen::Vector2d x;
+        Eigen::Vector2f x;
         int num_successful_walks = 0;
         for (int i = 0; i < num_walks; i++) {
-                Eigen::Vector2d x = x0;
-                float radius;
+                Eigen::Vector2f x = x0;
                 for (int steps = 0; steps < max_steps; ++steps) {
-                        radius = std::numeric_limits<float>::max();
+                        float radius = std::numeric_limits<float>::max();
                         for (int seg_idx = 0; seg_idx < bndary_segs.rows(); ++seg_idx) {
                                 radius = closest_point_on_segment(
                                   bndary_segs, x, seg_idx, radius);
@@ -108,19 +169,20 @@ static void
 update_Z(void)
 {
         if (use_camel) {
-                for (int Vidx = 0; Vidx < V.rows(); ++Vidx) {
-                }
+                const auto walk_on_spheres_parallel = [&](const int Vidx) {
+                        Z(Vidx) = walk_on_spheres(Vidx);
+                };
+                igl::parallel_for(V.rows(), walk_on_spheres_parallel, 1000);
         } else {
                 const auto walk_on_circles_parallel = [&](const int Vidx) {
-                        float u =
-                          walk_on_circles(Eigen::Vector2d{ V(Vidx, 0), V(Vidx, 1) },
+                        Z(Vidx) =
+                          walk_on_circles(Eigen::Vector2f{ V(Vidx, 0), V(Vidx, 1) },
                                           boundary_segments,
                                           boundary_condition,
                                           num_walks,
                                           max_steps);
-                        Z(Vidx) = u;
                 };
-                igl::parallel_for(V.rows(), walk_on_circles_parallel, 48);
+                igl::parallel_for(V.rows(), walk_on_circles_parallel, 1000);
         }
         ++num_updates;
 }
@@ -129,14 +191,16 @@ static bool
 key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier)
 {
         if (key == '1') {
-                Eigen::VectorXd Z_cma = Z;
+                Eigen::VectorXf Z_cma = Z;
                 update_Z();
                 Z = (((num_updates - 1) * Z_cma) + Z) / num_updates;
-                viewer.data().set_data(Z, igl::COLOR_MAP_TYPE_TURBO, 256);
+                viewer.data().set_data(
+                  Z.cast<double>(), igl::COLOR_MAP_TYPE_TURBO, 256);
         } else if (key == '2') {
                 num_updates = 0;
                 update_Z();
-                viewer.data().set_data(Z, igl::COLOR_MAP_TYPE_TURBO, 256);
+                viewer.data().set_data(
+                  Z.cast<double>(), igl::COLOR_MAP_TYPE_TURBO, 256);
         }
         return false;
 }
@@ -149,9 +213,6 @@ main(int argc, char** argv)
 
         options.add_options()                                                    //
           ("n,nvertices", "Num vertices (per edge)", cxxopts::value<uint32_t>()) //
-          ("l,do-linear-solve",
-           "Do linear solve?",
-           cxxopts::value<bool>()->default_value("false")) //
           ("e,exit-before-gui",
            "Exit before GUI",
            cxxopts::value<bool>()->default_value("false")) //
@@ -169,20 +230,20 @@ main(int argc, char** argv)
         auto result = options.parse(argc, argv);
 
         n_verts = result["nvertices"].as<uint32_t>();
-        bool do_linear_solve = result["do-linear-solve"].as<bool>();
         bool should_exit_before_gui = result["exit-before-gui"].as<bool>();
         use_camel = result["use-camel"].as<bool>();
         max_steps = result["max-steps"].as<int>();
         num_walks = result["num-walks"].as<int>();
 
         uint32_t n_edges = n_verts - 1;
-        double grid_spacing = 1.0 / n_verts;
+        float grid_spacing = 1.0 / n_verts;
 
         V.resize(n_verts * n_verts, 3);
         F.resize(2 * n_edges * n_edges, 3);
 
         if (use_camel) {
                 igl::readOFF("../camelhead.off", V, F);
+                igl::boundary_facets(F, boundary_edges);
         } else {
                 /* NOTE(brendan): plane */
                 for (int i = 0; i < n_verts; ++i) {
@@ -217,55 +278,6 @@ main(int argc, char** argv)
         }
 
         Z.resize(V.rows());
-        if (do_linear_solve) {
-                // Find boundary edges
-                Eigen::MatrixXi E;
-                igl::boundary_facets(F, E);
-
-                // Find boundary vertices
-                Eigen::VectorXi b, IA, IC;
-                igl::unique(E, b, IA, IC);
-
-                // List of all vertex indices
-                Eigen::VectorXi all, in;
-                igl::colon<int>(0, V.rows() - 1, all);
-
-                // List of interior indices
-                igl::setdiff(all, b, in, IA);
-
-                // Construct and slice up Laplacian
-                Eigen::SparseMatrix<double> L, L_in_in, L_in_b;
-                igl::cotmatrix(V, F, L);
-                igl::slice(L, in, in, L_in_in);
-                igl::slice(L, in, b, L_in_b);
-
-                // Dirichlet boundary conditions from x, y-coordinate
-                Eigen::VectorXd bc;
-                Z = V.col(0) + V.col(1);
-                igl::slice(Z, b, bc);
-
-                // Solve PDE
-                Eigen::SimplicialLLT<Eigen::SparseMatrix<double>> solver(-L_in_in);
-                Eigen::VectorXd Z_in = solver.solve(L_in_b * bc);
-
-                // slice into solution
-                igl::slice_into(Z_in, in, Z);
-
-                // Alternative, short hand
-                igl::min_quad_with_fixed_data<double> mqwf;
-
-                // Linear term is 0
-                Eigen::VectorXd B = Eigen::VectorXd::Zero(V.rows(), 1);
-
-                // Empty constraints
-                Eigen::VectorXd Beq;
-                Eigen::SparseMatrix<double> Aeq;
-
-                // Our cotmatrix is _negative_ definite, so flip sign
-                igl::min_quad_with_fixed_precompute((-L).eval(), b, Aeq, true, mqwf);
-                igl::min_quad_with_fixed_solve(mqwf, B, bc, Beq, Z);
-        }
-
         /* NOTE(brendan): alternative with monte carlo */
         update_Z();
 
@@ -277,8 +289,8 @@ main(int argc, char** argv)
 
         viewer.callback_key_down = &key_down;
 
-        viewer.data().set_mesh(V, F);
+        viewer.data().set_mesh(V.cast<double>(), F);
         viewer.data().show_lines = false;
-        viewer.data().set_data(Z, igl::COLOR_MAP_TYPE_TURBO, 256);
+        viewer.data().set_data(Z.cast<double>(), igl::COLOR_MAP_TYPE_TURBO, 256);
         viewer.launch();
 }
